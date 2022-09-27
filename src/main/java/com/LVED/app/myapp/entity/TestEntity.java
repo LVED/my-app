dfd80e4a6d35b6f3335feb66f1e1b3c9d0825897
Daniel Leon <daniel.leonv@telcel.com>
2022-09-27 12:53:05 -0500

    Nombre del proyecto: my-app. Descriocion: version final del proyecto my-app

---
+++ src/main/java/com/LVED/app/myapp/entity/TestEntity.java
@@ -0,0 +1,23 @@
+package com.LVED.app.myapp.entity;
+
+public class TestEntity {
+	private String nombre;
+
+	/***
+	 * Propiedad para asignar el valor a la variable nombre
+	 * @return
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/***
+	 * Propiedad para recuperar el valor a la variable nombre
+	 * @param nombre
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+}
